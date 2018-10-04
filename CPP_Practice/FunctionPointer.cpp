21158dcfc10a2c53714e9c5815967b3d540fc883
Chris <christopherjbbaker@gmail.com>
2018-10-04 12:55:17 -0400

    more testing

--- CPP_Practice/FunctionPointer.cpp
+++ CPP_Practice/FunctionPointer.cpp
@@ -14,8 +14,24 @@ int main()
 	*/
 	void(*pTest)(int) = test;
 
-
 	pTest(8);
+
+	void(*pTest2)(int);
+
+	//Get address of test by prefixing with ampersand
+	pTest2 = &test;
+
+	//Invoke function as if you were calling a function
+	pTest2(1);
+
+	//Or optionally dereference the function pointer
+	(*pTest2)(9);
+
+	void(*pTest3)(int);
+
+	//Get address of test by just naming it too
+	pTest3 = test;
+	pTest3(100);
 
 	std::cin.get();
 
