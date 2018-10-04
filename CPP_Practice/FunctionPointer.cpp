96a418aeacb6184950d4e44b6ec4dd9551514bd4
Chris <christopherjbbaker@gmail.com>
2018-10-04 12:29:52 -0400

    Add project files.

---
+++ CPP_Practice/FunctionPointer.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+void test(int val)
+{
+	std::cout << "Num: " << val << std::endl;
+}
+
+int main()
+{
+	test(7);
+
+	/*
+	void*pTest(int): a function which takes an int and returns void
+	*/
+	void(*pTest)(int) = test;
+
+	pTest(8);
+
+	std::cin.get();
+
+	return 0;
+}
